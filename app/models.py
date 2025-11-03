6cabf6fb533491500f2336576891e3ec53f8eca2
Jamie Montgomery <118894667+jamiemonty@users.noreply.github.com>
2025-11-03 17:24:50 +0000

    Added a GET and POST Endpoint

--- app/models.py
+++ app/models.py
@@ -1,6 +1,17 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import String, Integer
 
+
 class Base(DeclarativeBase):
     pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    student_id: Mapped[str] = mapped_column(String(8), unique=True, nullable=False)
 
